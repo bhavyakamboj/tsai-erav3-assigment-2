ad90f45daa9dbfe8c8cea40ee0d1d6394b8cc9fd
Bhavya Kamboj <bhavya.kamboj@gmail.com>
2024-11-10 13:04:44 +0530

    added hero section on home

--- app/operations/text/page.js
+++ app/operations/text/page.js
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <h1>Text Processing Page</h1>;
-  }
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+
+    // Hero section
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold text-gray-800 mb-4">Text Pre-Processing</h1>
+                <p className="text-lg text-gray-600 mb-8">Select the option to perform pre-processing operations</p>
+                
+                <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700">
+                    Get Started
+                </button>
+            </div>
+            
+        </div>
+    );
+}
